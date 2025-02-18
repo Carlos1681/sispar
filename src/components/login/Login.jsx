6e31f6a29a45b51c65e76133205fe52ac4d4e738
Carlos1681 <spjcarlosh@gmail.com>
2025-02-18 18:37:14 -0300

    Working on the login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,28 @@
+function Login() {
+  return (
+    <main>
+      <section></section>
+
+      <section>
+        <img src="" alt="" />
+        <h1>Boas vindas ao Novo Portal SISPAR</h1>
+        <p>Sistema de Emissão de Boletos e Parcelamento</p>
+
+        <form action="">
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+          />
+          <a href="">Esqueci minha senha</a>
+          <button>Entrar</button>
+          <button>Criar conta</button>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default Login;
