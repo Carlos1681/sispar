548bfb8998c58d5fe0b746e892b2459d3b3c4727
Carlos1681 <spjcarlosh@gmail.com>
2025-02-18 19:06:50 -0300

    Working on the refund component

---
+++ src/components/refunds/Refunds.jsx
@@ -0,0 +1,37 @@
+function Refunds() {
+  return (
+    <>
+      <header>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <p>Reembolso</p>
+      </header>
+      <main>
+        <section>
+          <h1>Sistema de Reembolsos</h1>
+          <p>
+            Solicite novos pedidos de reembolso, visualize solicitações em
+            análise e todo o histórico.
+          </p>
+          <article>
+            <img src="" alt="" />
+            <h3></h3>
+          </article>
+          <section>
+            <div>
+              <img src="" alt="" />
+              <h4></h4>
+              <p></p>
+            </div>
+          </section>
+          <section>
+            <img src="" alt="" />
+            <p>Sistema atualizado.</p>
+          </section>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Refunds;
