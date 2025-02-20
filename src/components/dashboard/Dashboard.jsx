a74d76ff47f5275de7df606737048ff3d8e22967
Carlos1681 <spjcarlosh@gmail.com>
2025-02-20 19:20:33 -0300

    Working on the navbar and the dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,69 @@
+import Navbar from "../navbar/Navbar.jsx";
+import Home from "../../assets/Dashboard/Vector (1).png";
+import Seta from "../../assets/Dashboard/Vector.png";
+import styles from "./Dahboard.module.scss";
+
 function Dashboard() {
-  return <></>;
+  return (
+    <div className={styles.layoutDashboard}>
+      <Navbar />
+
+      <div className={styles.mainContainer}>
+        <header className={styles.headerDashboard}>
+          <img src={Home} alt="Vetor de uma casinha" />
+          <img src={Seta} alt="Vetor de uma setinha" />
+          <p>Reembolsos</p>
+          <img src={Seta} alt="Vetor de uma setinha" />
+          <p>Solicitação de Reembolsos</p>
+        </header>
+
+        <main className={styles.mainDashboard}>
+          <form className={styles.formSolicitacao}>
+            <div className={styles.group1}>
+              <div className={styles.inputName}>
+                <label htmlFor="">Nome Completo</label>
+                <input type="text" name="" id="nome-completo" />
+              </div>
+
+              <div className={styles.inputCompany}>
+                <label htmlFor="">Empresa</label>
+                <input type="text" name="" id="empresa" />
+              </div>
+
+              <div className={styles.inputPayment}>
+                <label htmlFor="">Nº Prest. Contas</label>
+                <input type="text" name="" id="prestacao-contas" />
+              </div>
+
+              <div className={styles.inputMotive}>
+                <label htmlFor="">Descrição / Motivo do Reembolso</label>
+                <textarea name="" id="">
+                  {" "}
+                </textarea>
+              </div>
+            </div>
+
+            <div className={styles.verticalBar}></div>
+
+            <div className={styles.group2}>
+              <div className={styles.inputData}>
+                <label htmlFor="">Data</label>
+                <input type="date" name="" id="" />
+              </div>
+
+              <div className={styles.costs}>
+                <select name="" id="">
+                  <option value="">Selecionar</option>
+                  <option value=""> Alimentação </option>
+                  <option value=""> Combustível</option>
+                </select>
+              </div>
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const goToRefunds = () => {
+    navigate("/reembolsos");
+  };
+
   return (
     <main>
-      <section></section>
+      <section className={styles.containerLogin}></section>
 
-      <section>
-        <img src="" alt="" />
+      <section className={styles.containerForm}>
+        <img src={Logo} alt="Logo da wilson sons" />
+
         <h1>Boas vindas ao Novo Portal SISPAR</h1>
+
         <p>Sistema de Emissão de Boletos e Parcelamento</p>
 
         <form action="">
@@ -16,13 +28,15 @@ function Login() {
             id="password"
             placeholder="Senha"
           />
-          <a href="">Esqueci minha senha</a>
-          <button>Entrar</button>
-          <button>Criar conta</button>
+          <a href=""> Esqueci minha senha </a>
+
+          <div className={styles.buttonGroup}>
+            <button onClick={goToRefunds}> Entrar</button>
+            <button>Criar conta</button>
+          </div>
         </form>
       </section>
     </main>
   );
 }
-
 export default Login;
